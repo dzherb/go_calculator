ddb9c9d7d4799bd788fcbf074bbace9422a7185b
dzherb <zherbin.dima@yandex.ru>
2025-03-06 23:47:00 +0300

    add Dockerfile for client and update footer

--- client/src/components/AppFooter.vue
+++ client/src/components/AppFooter.vue
@@ -1,9 +1,15 @@
 <template>
-  <v-footer height="40" app>
-    aaaa
+  <v-footer height="40" app class="d-flex justify-center">
+    <span class="text-subtitle-2 text-grey-lighten-1">
+      &copy; 2025
+      <a
+        href="https://github.com/dzherb"
+        target="_blank"
+        class="ml-1 text-decoration-none text-green"
+      >dzherb</a>
+    </span>
   </v-footer>
 </template>
 
 <script setup>
-
 </script>
