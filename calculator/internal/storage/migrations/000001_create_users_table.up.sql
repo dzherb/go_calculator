37cc9858ca756bfe2a12d8274d89c53b72fe0490
Dmitrii Zherbin <zherbin.dima@yandex.ru>
2025-05-07 23:43:09 +0300

    migrations: add users table

---
+++ calculator/internal/storage/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,30 @@
+BEGIN;
+
+CREATE TABLE users
+(
+    id            SERIAL PRIMARY KEY,
+    username      VARCHAR(32) UNIQUE        NOT NULL,
+    password_hash CHAR(32)                  NOT NULL,
+    created_at    TIMESTAMPTZ DEFAULT now() NOT NULL,
+    updated_at    TIMESTAMPTZ DEFAULT now() NOT NULL
+);
+
+CREATE
+OR REPLACE FUNCTION set_updated_at()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    NEW.updated_at
+= now();
+RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER set_updated_at_trigger
+    BEFORE UPDATE
+    ON users
+    FOR EACH ROW
+    EXECUTE FUNCTION set_updated_at();
+
+COMMIT;
